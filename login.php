b28735ecbe0e670adbde158f8f875a7bfce9bd22
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-10-10 11:36:00 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,8 +1,10 @@
 <?php
 
+//Check for any errors
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
 
+//Grab required files 
 session_start();
 require_once('path.inc');
 require_once('get_host_info.inc');
@@ -16,21 +18,19 @@ else
 {
   $msg = "login";
 }
+//Get Username + Password
 $request = array();
 $request['type'] = "login";
-#$request['username'] = "user3@gmail.com";
-#$request['password'] = "1234";
 $request['username'] = $_POST['username'];
 $request['password'] = $_POST['pass'];
 $request['message'] = $msg;
 $response = $client->send_request($request);
-//$response = $client->publish($request);
 echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
+//If not in the database
 if ($response == 0 ) {
 	$date = date_create();
-	file_put_contents('events.log', "[".date_format($date, 'm-d-Y H:i:s')."]"." Login error for user with username: ".$_POST['username'].".\n", FILE_APPEND);
 	header("location:loginerror.html");
 }
 else {
@@ -41,7 +41,6 @@ else {
 	$user = mysqli_fetch_array($query,MYSQLI_ASSOC);
 	$_SESSION['userid'] = $user['username'];
 	$date = date_create();
-	file_put_contents('events.log', "[".date_format($date, 'm-d-Y H:i:s')."]"." User with username: ".$_POST['username']." logged in successfully.\n", FILE_APPEND);
 	header("Location: loginsuccess.html");
 }
 ?>
