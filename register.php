1aef4237352a703cf646f5a5d89d97a4ce056865
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-10-16 17:10:02 -0400

    Add files via upload

--- register.php
+++ register.php
@@ -30,37 +30,5 @@ $response = $client->send_request($request);
 echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
-
-//Check for errors again
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-ini_set('display_errors', 1);
-
-if {
-	
-	global $mydb;
-	//Check to see if the user is already in the database
-	$username = $_POST['username'];
-	$query = mysqli_query($mydb,"SELECT username FROM user WHERE username='$username'");
-	$count = mysqli_num_rows($query);
-	
-	//If pre-exsiting usernmae is found
-	if ($count == 1)
-	{
-		header("location:regerror.html");
-		return true;
-	}
-	//If username is not found insert into the table
-	else 
-	{
-		$query = mysqli_query($mydb, "INSERT INTO user (username, password) VALUES ('$username','$password')");
-		$user = mysqli_fetch_array($userQuery, MYSQLI_ASSOC);
-		header("location:regsuccess.html");
-		return false;
-	}
-//Catch for all other errors
-else ($response == 0 ) 
-{
-	$date = date_create();
-	header("location:regerror.html");
-}
+header("refresh:3;url=index.html");
 ?>
